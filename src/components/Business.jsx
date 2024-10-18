fa70af2cab36c63668000e651492418f039edcd2
sacha11 <s.j.g_97@hotmail.com>
2024-10-18 18:29:25 -0300

    reusable Button component done

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -7,9 +7,11 @@ const Business = () => {
     <section id='features' className={layout.section}>
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we'll handle the money.</h2>
-        <p className={`${styles.paragraph}`}>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           With the right credit card, you can improve your financial life by building credit, earning rewards and savign money. But with hundreds of cards on the market
         </p>
+
+        <Button  styles='mt-10'/>
       </div>
     </section>
   )
