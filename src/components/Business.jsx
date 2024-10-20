90922abe8166e13a0041f420d050fe27506cac75
sacha11 <s.j.g_97@hotmail.com>
2024-10-20 12:14:27 -0300

    feature component in dev for business component

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,6 +2,19 @@ import { features } from '../constants';
 import styles, { layout } from '../styles';
 import Button from './Button';
 
+const Feature = ({ icon, title, content, index }) => {
+  return (
+    <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+      <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+        <img src={icon} alt="icon"  className='w-[50%] h-[50%] object-contain'/>
+      </div>
+      <div className='flex-1 flex flex-col ml-3'>
+        <h4 className=''></h4>
+      </div>
+    </div>
+  )
+}
+
 const Business = () => {
   return (
     <section id='features' className={layout.section}>
