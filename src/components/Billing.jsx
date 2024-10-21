5dfb7f1b11bd5206fa2bb98d1016408ca41bda50
sacha11 <s.j.g_97@hotmail.com>
2024-10-21 11:31:53 -0300

    billing section done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -10,6 +10,17 @@ const Billing = () => {
         <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
         <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
       </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Easily control your <br className='sm:block hidden' /> billing & invoicing.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Effortlessly manage your billing and invoices with our business app, designed to streamline your financial operations. Track payments, generate invoices, and maintain clear records, all in one intuitive platform, giving you full control and visibility over your finances.
+        </p>
+        <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+          <img src={apple} alt="apple_store"  className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+          <img src={google} alt="google_store" className='w-[128px] h-[42px] object-contain mr-5 cursor-pointer' />
+        </div>
+      </div>
     </section>
   )
 }
